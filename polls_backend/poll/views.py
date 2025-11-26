92ee904dcf4fd0a8b549ad716b1c62c5b8a74b65
Ephraim Debel <ephraimdebel@gmail.com>
2025-11-26 23:28:43 +0300

    fix: wrong import file path corrected in the views.py and docker cmd update

--- polls_backend/poll/views.py
+++ polls_backend/poll/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets, generics, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from polls_backend.poll.utils import rate_limit
+from poll.utils import rate_limit
 from .models import Poll, Question, Choice, Vote
 from .serializers import (
     PollListSerializer, PollDetailSerializer,
